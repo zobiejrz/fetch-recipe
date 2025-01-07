e4b7dbc9c117c4ea40642e46660cea778d1de6e5
Ben Zobrist <ben@zobie.com>
2025-01-07 13:08:28 -0500

    Updated RecipeDetailView
    - Added basic struct to load JSON into
    - Updated RecipeDetailView to show more data from the struct

---
+++ fetch-recipe/fetch-recipe/Model/RecipeStruct.swift
@@ -0,0 +1,26 @@
+//
+//  RecipeStruct.swift
+//  fetch-recipe
+//
+//  Created by Ben Zobrist on 1/7/25.
+//
+
+import Foundation
+
+struct Root: Decodable {
+    let recipes: [Recipe]
+    
+    enum CodingKeys : String, CodingKey { case recipes = "recipes" }
+}
+
+struct Recipe: Decodable {
+    
+    let cuisine: String
+    let name: String
+    let photo_url_large: String?
+    let photo_url_small: String?
+    let uuid: String
+    let source_url: String?
+    let youtube_url: String?
+
+}
--- fetch-recipe/fetch-recipe/Views/RecipeDetailView.swift
+++ fetch-recipe/fetch-recipe/Views/RecipeDetailView.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct RecipeDetailView: View {
+    
+    let recipe: Recipe
+    
     var body: some View {
-        Text("Recipe Details")
+        VStack(alignment: .leading) {
+            Divider()
+            
+            Text("\(recipe.cuisine) cuisine")
+                .font(.subheadline)
+            
+            Image(systemName: "photo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding()
+
+            Divider()
+            
+            if let source_url = recipe.source_url {
+                Link(destination: URL(string: source_url)!) {
+                    Text("Source URL ") + Text(Image(systemName: "link"))
+                }
+            }
+            
+            if let youtube_url = recipe.youtube_url {
+                Link(destination: URL(string: youtube_url)!) {
+                    Text("YouTube URL ") + Text(Image(systemName: "link"))
+                }
+            }
+            
+            Spacer()
+        }
+        .padding()
+        .navigationTitle(recipe.name)
     }
 }
 
 #Preview {
-    RecipeDetailView()
+    NavigationView {
+        RecipeDetailView(recipe:
+                            Recipe(
+                                cuisine: "Malaysian",
+                                name: "Apam Balik",
+                                photo_url_large: "https://d3jbb8n5wk0qxi.cloudfront.net/photos/b9ab0071-b281-4bee-b361-ec340d405320/large.jpg",
+                                photo_url_small: "https://d3jbb8n5wk0qxi.cloudfront.net/photos/b9ab0071-b281-4bee-b361-ec340d405320/small.jpg",
+                                uuid: "0c6ca6e7-e32a-4053-b824-1dbf749910d8",
+                                source_url: nil,
+                                youtube_url: "https://www.youtube.com/watch?v=6R8ffRRJcrg"
+                            )
+        )
+    }
 }
