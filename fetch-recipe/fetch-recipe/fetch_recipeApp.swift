268978befe2ef183eeafcda2cd5ca0bc6227b28d
Ben Zobrist <ben@zobie.com>
2025-01-08 17:58:59 -0500

    Added Unit Tests for ImageCacheManager

--- fetch-recipe/fetch-recipe/fetch_recipeApp.swift
+++ fetch-recipe/fetch-recipe/fetch_recipeApp.swift
@@ -8,6 +8,22 @@
 import SwiftUI
 
 @main
+struct AppLauncher {
+    static func main() throws {
+        if NSClassFromString("XCTestCase") == nil {
+            fetch_recipeApp.main()
+        } else {
+            TestApp.main()
+        }
+    }
+}
+
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup { Text("Running Unit Tests") }
+    }
+}
+
 struct fetch_recipeApp: App {
     let persistenceController = PersistenceController.shared
 
--- fetch-recipe/fetch-recipeTests/fetch_recipeTests.swift
+++ fetch-recipe/fetch-recipeTests/fetch_recipeTests.swift
@@ -6,12 +6,58 @@
 //
 
 import Testing
+import Foundation
+import UIKit
 @testable import fetch_recipe
 
 struct fetch_recipeTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    
+    @Test func testAddToCache() async throws {
+        ImageCacheManager.instance.removeAll()
+        #expect(ImageCacheManager.instance.numStored == 0, "ImageCacheManager needs to start empty")
+        
+        let tmp = UIImage(systemName: "swift")!
+        let res = ImageCacheManager.instance.add(image: tmp, key: "1")
+        
+        #expect(res == "ADDED TO CACHE", "Got \(res), expected 'ADDED TO CACHE'")
+        #expect(ImageCacheManager.instance.numStored == 1, "Only one image should be cached")
+        
+        ImageCacheManager.instance.removeAll()
     }
-
+    
+    @Test func testGetFromCache() async throws {
+        ImageCacheManager.instance.removeAll()
+        #expect(ImageCacheManager.instance.numStored == 0, "ImageCacheManager needs to start empty")
+        
+        let input = UIImage(systemName: "swift")!
+        var res = ImageCacheManager.instance.add(image: input, key: "1")
+        
+        #expect(res == "ADDED TO CACHE", "Got \(res), expected 'ADDED TO CACHE'")
+        #expect(ImageCacheManager.instance.numStored == 1, "Only one image should be cached")
+        
+        let output = ImageCacheManager.instance.get(key: "1")
+        
+        #expect(output == input, "Didn't get the same image back as was cached")
+        ImageCacheManager.instance.removeAll()
+    }
+    
+    @Test func testRemoveFromCache() async throws {
+        ImageCacheManager.instance.removeAll()
+        #expect(ImageCacheManager.instance.numStored == 0, "ImageCacheManager needs to start empty")
+        
+        let input = UIImage(systemName: "swift")!
+        var res = ImageCacheManager.instance.add(image: input, key: "1")
+        
+        #expect(res == "ADDED TO CACHE", "Got \(res), expected 'ADDED TO CACHE'")
+        #expect(ImageCacheManager.instance.numStored == 1, "Only one image should be cached")
+        
+        let output = ImageCacheManager.instance.get(key: "1")
+        
+        #expect(output == input, "Didn't get the same image back as was cached")
+        
+        res = ImageCacheManager.instance.remove(key: "1")
+        
+        #expect(res == "REMOVE FROM CACHE", "Got \(res), expected 'REMOVE FROM CACHE'")
+        #expect(ImageCacheManager.instance.numStored == 0, "No images should be cached")
+    }
 }
